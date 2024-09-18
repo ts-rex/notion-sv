cdd2a1aa3136aa25a7c8c36a6119ff53c9b61881
mavdotjs <consorceo300@icloud.com>
2024-09-18 19:45:31 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ Render Notion markdown representation using svelte 5 snippets!
 
 ## Intended use:
 ```svelte
-<RenderNotion>
+<h1>{page.title}</h1>
+<RenderNotion ignore-unmarked {blocks}>
+{#snippet heading(importance, richText)}
+{#if importance == 1}
+<h1>{@render richText()}</h1>
+{:else if importance == 2}
+<h2>{@render richText()}</h2>
+{:else if importance == 3}
+<h3>{@render richText()}</h3>
+{/if}
+{/snippet}
 </RenderNotion>
 ```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<h1>{page.title}</h1>
+<RenderNotion ignore-unmarked {blocks} getChildren={id => fetchBlockFromYourAPI(id)}>
+{#snippet heading(importance, richText)}
+    {#if importance == 1}
+        <h1>{@render richText()}</h1>
+    {:else if importance == 2}
+        <h2>{@render richText()}</h2>
+    {:else if importance == 3}
+        <h3>{@render richText()}</h3>
+    {/if}
+{/snippet}
+{#snippet divider()}
+<hr/>
+{/snippet}
+</RenderNotion>
